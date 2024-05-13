06ff5219924037e9c3adcd691e7fb7da2dc27ff0
yilmazdikilitas <yilmazdikilitas91@gmail.com>
2024-05-13 18:15:01 +0300

    Update contentDetails2.js

--- contentDetails2.js
+++ contentDetails2.js
@@ -43,7 +43,7 @@ function dynamicContentDetails(ob)
     detailsDiv.id = 'details'
 
     let h3DetailsDiv = document.createElement('h3')
-    let h3DetailsText = document.createTextNode('TL 6800 ')
+    let h3DetailsText = document.createTextNode('TL 6700 ')
     //let h3DetailsText = document.createTextNode('TL 2800 ' + ob.price)
     h3DetailsDiv.appendChild(h3DetailsText)
 
